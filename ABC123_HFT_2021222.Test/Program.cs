154b354769de4e757ccdac6cc169534ba984caf9
adajanos02 <adamecz36@gmail.com>
2022-10-18 17:09:13 +0200

    First part(solutions)

---
+++ ABC123_HFT_2021222.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC123_HFT_2021222.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
